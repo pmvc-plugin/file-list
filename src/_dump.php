ef2966112d80fd67228f3cf6492ac75ec65e578a
Hill <hill@kimo.com>
2018-09-18 18:04:38 +0000

    improve process header

--- src/_dump.php
+++ src/_dump.php
@@ -4,13 +4,16 @@ ${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\dump';
 
 class dump
 {
-    function __invoke($filename, $setHeader = true)
+    function __invoke($filename, $setHeader = true, $dumpOnEmptyHeader = true)
     {
+        $header = [];
         if ($setHeader) {
-          $this->_processHeader($filename);
+          $header = $this->_processHeader($filename);
         }
         $this->_cleanBuffer();
-        readfile($filename);
+        if ($dumpOnEmptyHeader || !empty($header)) {
+          readfile($filename);
+        }
     }
 
     private function _cleanBuffer()
@@ -27,6 +30,23 @@ class dump
         $contentType = \PMVC\plug('file_info')
             ->path($filename) 
             ->getContentType();
-        header('Content-Type: '.$contentType);
+        $header = ['Content-Type: '.$contentType];
+        \PMVC\dev(function() use (&$header){
+            $old = $header;
+            $header = [];
+            return $old;
+        }, 'dump');
+        if (defined('_ROUTER')) {
+          \PMVC\callPlugin(
+            \PMVC\getOption(_ROUTER),
+            'processHeader',
+            [$header]
+          );
+        } else {
+          if (!empty($header[0])) {
+            header($header[0]);
+          }
+        }
+        return $header;
     }
 }
